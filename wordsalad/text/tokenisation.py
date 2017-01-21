c05e58b821828afd349437422c0b8ec0d1d2b87f
Simon Otter <skurmedel@gmail.com>
2017-01-21 19:39:24 +0100

    implements the splitWestern function

--- wordsalad/text/tokenisation.py
+++ wordsalad/text/tokenisation.py
@@ -4,9 +4,31 @@ def splitWestern(text, strip={"\r"," ", "\n", "\t"}, whitespace=string.whitespac
     """Tries to split the input text as if it were natural language "western" text,
     like english, french or german.
 
-    Whitespace will be used as word separators, characters in strip will be removed
+    whitespace will be used as word separators, characters in strip will be removed
     and punctuation will be treated like single, one character words.
 
     Returns a generator.
     """
-    pass
+    whitespace = str(whitespace)
+    if whitespace == "":
+        raise ValueError("whitespace is empty, I have nothing to split on.")
+    
+    nonWordChars = whitespace + punctuation
+    i = 0
+    while i < len(text):
+        c = text[i]
+        if c in whitespace:
+            while i < len(text) and text[i] in whitespace:
+                i+=1
+        elif c not in punctuation:
+            word = ""
+            while i < len(text) and text[i] not in nonWordChars:
+                word += text[i]
+                i+=1
+            if word != "":
+                yield word
+        else:
+            yield c
+            i+=1
+
+            
