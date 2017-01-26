284f0f7951c69a9e7a16100fc740d5f58bbd1816
Simon Otter <skurmedel@gmail.com>
2017-01-26 02:33:36 +0100

    Documented the cases better.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,21 +6,25 @@ class TestJoinGermanic(unittest.TestCase):
     def test_joinGermanic(self):
         cases = [
             (
+                # No concat or quoting unless told to.
                 ["abc", "'", "abc", "'"],
                 "abc ' abc '",
-                {"quoteChars": ""}
+                {"quoteChars": "", "concat": ""}
             ),
             (
+                # Test quotes and non-strings.
                 [1, 2, 3, "\"", "hey", "\""],
                 "1 2 3 \"hey\"",
                 {"quoteChars": "\""}
             ),
             (
+                # Make sure ellipsis are handled decently.
                 ["abracadabra", ".", ".", ".", "abracadabra"],
                 "abracadabra... Abracadabra",
                 {"capitalize": True}
             ),
             (
+                # Todo: We want capitalization to work after "?" too.
                 ["Whose", "burger", "is", "that", "?", "john", "'", "s", "."],
                 "Whose burger is that? john's.",
                 {"capitalize": True, "concat":"'"}
