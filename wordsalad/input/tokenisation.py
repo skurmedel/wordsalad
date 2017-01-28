49cd3c64824a50d1f85a0000b81315b456cd04e0
Simon Otter <skurmedel@gmail.com>
2017-01-29 00:46:39 +0100

    start_words param implemented.

--- wordsalad/input/tokenisation.py
+++ wordsalad/input/tokenisation.py
@@ -1,6 +1,6 @@
 import string
 
-def split_germanic(text, strip={"\r"," ", "\n", "\t"}, whitespace=string.whitespace, punctuation=string.punctuation, start_words=[]):
+def split_germanic(text, strip={"\r"," ", "\n", "\t"}, whitespace=string.whitespace, punctuation=string.punctuation, start_words=[], sentence_end=".?!"):
     """Tries to split the input text as if it were natural language "germanic" text,
     like english or german.
 
@@ -15,20 +15,25 @@ def split_germanic(text, strip={"\r"," ", "\n", "\t"}, whitespace=string.whitesp
     
     nonWordChars = whitespace + punctuation
     i = 0
+    prevWasSentenceEnd = True
     while i < len(text):
         c = text[i]
         if c in whitespace:
             while i < len(text) and text[i] in whitespace:
                 i+=1
-        elif c not in punctuation:
+        elif c not in punctuation and c not in sentence_end:
             word = ""
             while i < len(text) and text[i] not in nonWordChars:
                 word += text[i]
                 i+=1
             if word != "":
                 yield word
+                if prevWasSentenceEnd:
+                    start_words.append(word)
+                prevWasSentenceEnd = False
         else:
             yield c
+            prevWasSentenceEnd = True
             i+=1
 
             
